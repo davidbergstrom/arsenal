b9a394929a2d8712e720c878fc23a14f66c46966
jcberntsson <jcberntsson@gmail.com>
2014-09-29 16:04:49 +0200

    Added Map class and Milestone Interface

---
+++ app/src/main/java/com/edit/reach/model/Map.java
@@ -0,0 +1,15 @@
+package com.edit.reach.model;
+
+import java.util.List;
+
+/**
+ * Created by Joakim Berntsson on 2014-09-29.
+ * Class containing all logic for handling map actions.
+ */
+public class Map {
+	private List<Milestone> milestones;
+
+	public Map(){
+
+	}
+}
---
+++ app/src/main/java/com/edit/reach/model/Milestone.java
@@ -0,0 +1,10 @@
+package com.edit.reach.model;
+
+/**
+ * Created by Joakim Berntsson on 2014-09-29.
+ * Interface for Milestones.
+ *
+ */
+public interface Milestone {
+
+}
