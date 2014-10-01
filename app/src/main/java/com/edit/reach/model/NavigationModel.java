63c205cd574b23825a91ac3e391977f3557fad54
Tim Kerschbaumer <tim.kersch@gmail.com>
2014-10-01 23:22:30 +0200

    Updates in the VehicleSystem. Also built a solution that makes it possible to both get data when you want and get data when things changes in the truck. Improved communication between NavigationModel and VehicleSystem.

--- app/src/main/java/com/edit/reach/model/NavigationModel.java
+++ app/src/main/java/com/edit/reach/model/NavigationModel.java
@@ -1,24 +1,99 @@
 package com.edit.reach.model;
 
+
+import android.swedspot.automotiveapi.AutomotiveSignalId;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * Created by: Tim Kerschbaumer
  * Project: REACH
  * Date: 2014-09-27
  * Time: 19:27
- * Last Edit:
+ * Last Edit: 2014-10-01
  */
 // TODO Everything that has to do with the stationairy view
-public class NavigationModel {
+// TODO Run this class in separate thread.
+public class NavigationModel implements Observer {
 
 	private final VehicleSystem vehicleSystem;
 
+	// TODO
+	/** Constructor...
+	 */
 	public NavigationModel() {
+		List<Integer> signalList = new ArrayList<Integer>();
 		vehicleSystem = new VehicleSystem();
+		vehicleSystem.addObserver(this);
 	}
 
-	public VehicleSystem getVehicleSystem() {
-		return vehicleSystem;
+	/** Do not call this method. It is called automatically when the observable changes.
+	 * @param observable Not used
+	 * @param data The id of the signal that initiated this update.
+	 */
+	@Override
+	public void update(Observable observable, Object data) {
+		int id;
+		if(data.getClass() == Integer.TYPE) {
+			id = (Integer)data;
+
+			switch (id) {
+				// How much fuel is left in tank.
+				case AutomotiveSignalId.FMS_FUEL_LEVEL_1:
+					// TODO do stuff
+					break;
+
+				// Is vehicle moving
+				case AutomotiveSignalId.FMS_VEHICLE_MOTION:
+					// TODO do stuff
+					break;
+
+				// Instantanious Fuelconsumption
+				case AutomotiveSignalId.FMS_FUEL_RATE:
+					// TODO do stuff
+					break;
+
+				// Instantanious Fueleconomy
+				case AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY:
+					// TODO do stuff
+					break;
+
+				// Distance to service
+				case AutomotiveSignalId.FMS_SERVICE_DISTANCE:
+					// TODO do stuff
+					break;
+
+				// Vehicle speed (Tachograph)
+				case AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED:
+					// TODO do stuff
+					break;
+
+				// Moving direction
+				case AutomotiveSignalId.FMS_DIRECTION_INDICATOR:
+					// TODO do stuff
+					break;
+
+				// Has a driver
+				case AutomotiveSignalId.FMS_DRIVER_1_CARD:
+					// TODO do stuff
+					break;
+
+				// Working state of driver
+				case AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE:
+					// TODO do stuff
+					break;
+
+				// Time in states of driver
+				case AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES:
+					// TODO do stuff
+					break;
+			}
+		} else {
+			Log.d("OBSERVER", "Observer called without any correct ID");
+		}
 	}
-
-
 }
--- app/src/main/java/com/edit/reach/model/VehicleSystem.java
+++ app/src/main/java/com/edit/reach/model/VehicleSystem.java
@@ -2,7 +2,6 @@ package com.edit.reach.model;
 
 import android.swedspot.automotiveapi.AutomotiveSignal;
 import android.swedspot.automotiveapi.AutomotiveSignalId;
-import android.swedspot.scs.SCS;
 import android.swedspot.scs.data.*;
 import android.util.Log;
 import com.swedspot.automotiveapi.AutomotiveFactory;
@@ -12,106 +11,200 @@ import com.swedspot.vil.distraction.DriverDistractionLevel;
 import com.swedspot.vil.distraction.DriverDistractionListener;
 import com.swedspot.vil.policy.AutomotiveCertificate;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Observable;
+
 /**
  * Created by: Tim Kerschbaumer
  * Project: REACH
  * Date: 2014-09-27
  * Time: 19:28
- * Last Edit: 2014-09-30
+ * Last Edit: 2014-10-01
  */
-public class VehicleSystem {
+class VehicleSystem extends Observable {
 
 	/* --- Instance Variables --- */
-	private SCSFloat fuelLevel;
-	private SCSFloat instantFuelConsumption;
-	private SCSFloat instantFuelEconomy;
-	private SCSFloat vehicleSpeed;
-	private SCSInteger distanceToService;
-	private Uint8 movingDirection;
-	private Uint8 isMoving;
+	private SCSFloat fuelLevel = new SCSFloat(-1f);
+	private SCSFloat instantFuelConsumption = new SCSFloat(-1f);
+	private SCSFloat instantFuelEconomy = new SCSFloat(-1f);
+	private SCSFloat vehicleSpeed = new SCSFloat(-1f);
+
+	private Uint8 movingDirection = new Uint8(-1);
+	private Uint8 isMoving = new Uint8(-1);
+	private Uint8 card = new Uint8(-1);
+	private Uint8 workingState = new Uint8(-1);
+	private Uint8 timeRelativeWorkingState = new Uint8(-1);
+
+	private SCSInteger distanceToService = new SCSInteger(-1);
+
+	private SCSInteger totalVehicleDistance;
+
+	private SCSDouble totalFuelUsed;
+
+	private SCSLong totalHoursOfOperation;
+
+	private Uint16 totalWeight;
+
+	private SCSString vehicleIdNumber;
+
+	private long driveStartTime = -1;
+	private long driveStopTime = -1;
+	private long downTime = -1;
+
+	private List<Integer> signalIDs;
 
 	// TODO Null? What to have in constructor
 	private final AutomotiveCertificate automotiveCertificate = new AutomotiveCertificate(null);
 
+	// TODO What to do here?
+	private final DriverDistractionListener driverDistractionListener = new DriverDistractionListener() {
+		@Override
+		public void levelChanged(DriverDistractionLevel driverDistractionLevel) {
+		}
+	};
+
 	private final AutomotiveListener automotiveListener = new AutomotiveListener() {
 		@Override
 		public void receive(AutomotiveSignal automotiveSignal) {
+
 			// Incoming signal
 			switch (automotiveSignal.getSignalId()) {
 
 				// How much fuel is left in tank.
 				case AutomotiveSignalId.FMS_FUEL_LEVEL_1:
-					setFuel((SCSFloat)(automotiveSignal.getData()));
+					SCSFloat prevFuelLevel = fuelLevel;
+					fuelLevel = (SCSFloat)(automotiveSignal.getData());
+
+					if(isRegistered(AutomotiveSignalId.FMS_FUEL_LEVEL_1) && prevFuelLevel.getFloatValue() != fuelLevel.getFloatValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_FUEL_LEVEL_1);
+					}
 					break;
 
 				// Is vehicle moving
 				case AutomotiveSignalId.FMS_VEHICLE_MOTION:
-					setMoving((Uint8)(automotiveSignal.getData()));
+					Uint8 prevIsMoving = isMoving;
+					isMoving = (Uint8)(automotiveSignal.getData());
+
+					if(isRegistered(AutomotiveSignalId.FMS_VEHICLE_MOTION) && prevIsMoving.getIntValue() != isMoving.getIntValue()) {
+						// TODO setTime();
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_VEHICLE_MOTION);
+					}
 					break;
 
 				// Instantanious Fuelconsumption
 				case AutomotiveSignalId.FMS_FUEL_RATE:
-					setInstantaneousFuelConsumption((SCSFloat)automotiveSignal.getData());
+					SCSFloat prevFuelRate = instantFuelConsumption;
+					instantFuelConsumption = (SCSFloat)automotiveSignal.getData();
+
+					if(isRegistered(AutomotiveSignalId.FMS_FUEL_RATE) && prevFuelRate.getFloatValue() != instantFuelConsumption.getFloatValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_FUEL_RATE);
+					}
 					break;
 
 				// Instantanious Fueleconomy
 				case AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY:
-					setInstantFuelEconomy((SCSFloat)automotiveSignal.getData());
+					SCSFloat prevFuelEconomy = instantFuelEconomy;
+					instantFuelEconomy = (SCSFloat)automotiveSignal.getData();
+
+					if(isRegistered(AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY) && prevFuelEconomy.getFloatValue() != instantFuelEconomy.getFloatValue() ) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY);
+					}
 					break;
 
 				// Distance to service
 				case AutomotiveSignalId.FMS_SERVICE_DISTANCE:
-					setDistanceToService((SCSInteger)(automotiveSignal.getData()));
+					SCSInteger prevDistanceToService = distanceToService;
+					distanceToService = (SCSInteger)(automotiveSignal.getData());
+
+					if(isRegistered(AutomotiveSignalId.FMS_SERVICE_DISTANCE) && prevDistanceToService.getIntValue() != distanceToService.getIntValue() ) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_SERVICE_DISTANCE);
+					}
 					break;
 
 				// Vehicle speed (Tachograph)
 				case AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED:
-					setVehicleSpeed((SCSFloat)automotiveSignal.getData());
-					break;
+					SCSFloat prevVehicleSpeed = vehicleSpeed;
+					vehicleSpeed = ((SCSFloat)automotiveSignal.getData());
 
-				// Vehicle speed (Wheels)
-				case AutomotiveSignalId.FMS_WHEEL_BASED_SPEED:
-					setVehicleSpeed((SCSFloat)automotiveSignal.getData());
+					if(isRegistered(AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED) && prevVehicleSpeed.getFloatValue() != vehicleSpeed.getFloatValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED);
+					}
 					break;
 
 				// Moving direction
 				case AutomotiveSignalId.FMS_DIRECTION_INDICATOR:
-					setMovingDirection((Uint8)automotiveSignal.getData());
+					Uint8 prevMovingDirection = movingDirection;
+					movingDirection = ((Uint8)automotiveSignal.getData());
+
+					if(isRegistered(AutomotiveSignalId.FMS_DIRECTION_INDICATOR) && prevMovingDirection.getIntValue() != movingDirection.getIntValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_DIRECTION_INDICATOR);
+					}
 					break;
 
-				// Identification of driver1 / driver2
-				case AutomotiveSignalId.FMS_DRIVER_1_DRIVER_2_IDENTIFICATION:
-					// TODO
-					break;
-
-				// Has a driver in slot 1.
+				// Has a driver
 				case AutomotiveSignalId.FMS_DRIVER_1_CARD:
-					// TODO
+					Uint8 prevCard = card;
+					card = (Uint8)automotiveSignal.getData();
+
+					if(isRegistered(AutomotiveSignalId.FMS_DRIVER_1_CARD) && prevCard.getIntValue() != card.getIntValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_DRIVER_1_CARD);
+					}
 					break;
 
-				// Working state of driver in slot 1.
+				// Working state of driver
 				case AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE:
-					// TODO
+					Uint8 prevWorkingState = workingState;
+					workingState = (Uint8)automotiveSignal.getData();
+
+					if(isRegistered(AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE) && prevWorkingState.getIntValue() != workingState.getIntValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE);
+					}
 					break;
 
-				// Time in states of driver in slot 1.
+				// Time in states of driver
 				case AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES:
-					// TODO
+					Uint8 prevTimeRWS = timeRelativeWorkingState;
+					timeRelativeWorkingState = (Uint8)automotiveSignal.getData();
+
+					if(isRegistered(AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES) && prevTimeRWS.getIntValue() != timeRelativeWorkingState.getIntValue()) {
+						setChanged();
+						notifyObservers(AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES);
+					}
 					break;
 
-				// Has a driver in slot 2.
-				case AutomotiveSignalId.FMS_DRIVER_2_CARD:
-					// TODO
+				// The identification number of the vehicle
+				case AutomotiveSignalId.FMS_VEHICLE_IDENTIFICATION_NUMBER:
+					vehicleIdNumber = (SCSString)automotiveSignal.getData();
 					break;
 
-				// Working state of driver in slot 2.
-				case AutomotiveSignalId.FMS_DRIVER_2_WORKING_STATE:
-					// TODO
+				// Total hours of operation
+				case AutomotiveSignalId.FMS_ENGINE_TOTAL_HOURS_OF_OPERATION:
+					totalHoursOfOperation = (SCSLong)automotiveSignal.getData();
 					break;
 
-				// Time in states of driver in slot 2.
-				case AutomotiveSignalId.FMS_DRIVER_2_TIME_REL_STATES:
-					// TODO
+				// Total fuel used
+				case AutomotiveSignalId.FMS_HIGH_RESOLUTION_ENGINE_TOTAL_FUEL_USED:
+					totalFuelUsed = (SCSDouble)automotiveSignal.getData();
+					break;
+
+				// Total vehicle distance
+				case AutomotiveSignalId.FMS_HIGH_RESOLUTION_TOTAL_VEHICLE_DISTANCE:
+					totalVehicleDistance = (SCSInteger)automotiveSignal.getData();
+					break;
+
+				// Total weight of the vehicle
+				case AutomotiveSignalId.FMS_GROSS_COMBINATION_VEHICLE_WEIGHT:
+					totalWeight = (Uint16)automotiveSignal.getData();
 					break;
 
 				default:
@@ -131,24 +224,69 @@ public class VehicleSystem {
 		}
 	};
 
-	private final DriverDistractionListener driverDistractionListener = new DriverDistractionListener() {
-		@Override
-		public void levelChanged(DriverDistractionLevel driverDistractionLevel) {
-			// TODO what?
-		}
-	};
-
 	private final AutomotiveManager manager = AutomotiveFactory.createAutomotiveManagerInstance(automotiveCertificate, automotiveListener, driverDistractionListener);
 
+	/* --- CONSTANTS --- */
+	private static final long GIGA_CONSTANT = (long)Math.pow(10,9);
+	private static final long APPROVED_STOP = 900;
+	private static final long TO_MINUTES = 60;
+	private static final long THRESHOLD = GIGA_CONSTANT*APPROVED_STOP;
+
+	// TODO
+	/** Default empty constructor.
+	 */
 	public VehicleSystem() {
-		// TODO Which signals to register?
+	}
+
+	/** Constructor that registers listeners for ID:s given as parameter.
+	 * @param identities The signal ID:s to be listened to.
+	 */
+	public VehicleSystem(List<Integer> identities) {
+		signalIDs = identities;
+		this.registerListeners(signalIDs);
+	}
+
+	/** Register listeners for identities in the list.
+	 * @param identities the ID:s for the signals to be listened to.
+	 */
+	public void registerListeners(List<Integer> identities) {
+		if(signalIDs != null) {
+			Iterator<Integer> it = identities.iterator();
+			while(it.hasNext()) {
+				if(!signalIDs.contains(it.next())) {
+					signalIDs.add(it.next());
+				}
+			}
+		} else {
+			signalIDs = identities;
+		}
+
+		Iterator<Integer> it = identities.iterator();
+		while(it.hasNext()) {
+			manager.register((int)it.next());
+		}
+	}
+
+	/** Unregister listeners for ID:s in the list.
+	 * @param identities the ID:S for the signals to be unregistered from listening.
+	 */
+	public void unregisterListeners(List<Integer> identities) {
+		Iterator<Integer> it = identities.iterator();
+		while(it.hasNext()) {
+			if(signalIDs.contains(it.next())) {
+				manager.unregister((int)it.next());
+				signalIDs.remove((int) it.next());
+			}
+		}
 	}
 
 	/** Gets the amount of fuel in the vehicles tank.
 	 * @return A float with Precent of fuel left in tank.
 	 */
 	public float getFuelLevel() {
-		manager.requestValue(AutomotiveSignalId.FMS_FUEL_LEVEL_1);
+		if(!isRegistered(AutomotiveSignalId.FMS_FUEL_LEVEL_1)) {
+			manager.requestValue(AutomotiveSignalId.FMS_FUEL_LEVEL_1);
+		}
 		return fuelLevel.getFloatValue();
 	}
 
@@ -156,7 +294,9 @@ public class VehicleSystem {
 	 * @return false if not moving, true if moving
 	 */
 	public boolean isMoving() {
-		manager.requestValue(AutomotiveSignalId.FMS_VEHICLE_MOTION);
+		if(!isRegistered(AutomotiveSignalId.FMS_VEHICLE_MOTION)) {
+			manager.requestValue(AutomotiveSignalId.FMS_VEHICLE_MOTION);
+		}
 		int moving = isMoving.getIntValue();
 		return(moving != 0);
 	}
@@ -165,7 +305,9 @@ public class VehicleSystem {
 	 * @return a float with the consumption in L/h
 	 */
 	public float getInstantFuelConsumption() {
-		manager.requestValue(AutomotiveSignalId.FMS_FUEL_RATE);
+		if(!isRegistered(AutomotiveSignalId.FMS_FUEL_RATE)) {
+			manager.requestValue(AutomotiveSignalId.FMS_FUEL_RATE);
+		}
 		return instantFuelConsumption.getFloatValue();
 	}
 
@@ -173,7 +315,9 @@ public class VehicleSystem {
 	 * @return a float with the current economy in km/l
 	 */
 	public float getInstantFuelEconomy() {
-		manager.requestValue(AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY);
+		if(!isRegistered(AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY)) {
+			manager.requestValue(AutomotiveSignalId.FMS_INSTANTANEOUS_FUEL_ECONOMY);
+		}
 		return instantFuelEconomy.getFloatValue();
 	}
 
@@ -181,7 +325,9 @@ public class VehicleSystem {
 	 * @return a int with the number of km to next service.
 	 */
 	public int getDistanceToService() {
-		manager.requestValue(AutomotiveSignalId.FMS_SERVICE_DISTANCE);
+		if(!isRegistered(AutomotiveSignalId.FMS_SERVICE_DISTANCE)) {
+			manager.requestValue(AutomotiveSignalId.FMS_SERVICE_DISTANCE);
+		}
 		return distanceToService.getIntValue();
 	}
 
@@ -189,8 +335,10 @@ public class VehicleSystem {
 	 * @return a float with the current speed of the vehicle in km/h
 	 */
 	public float getVehicleSpeed() {
-		// TODO Check that tachograph is working properly.
-		manager.requestValue(AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED);
+		// TODO Check that tachograph is working properly. If not, take wheel speed.
+		if(!isRegistered(AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED)) {
+			manager.requestValue(AutomotiveSignalId.FMS_TACHOGRAPH_VEHICLE_SPEED);
+		}
 		return vehicleSpeed.getFloatValue();
 	}
 
@@ -198,38 +346,114 @@ public class VehicleSystem {
 	 * @return a int with 0 if moving forward and 1 if moving reverse.
 	 */
 	public int getMovingDirection() {
-		manager.requestValue(AutomotiveSignalId.FMS_DIRECTION_INDICATOR);
+		if(!isRegistered(AutomotiveSignalId.FMS_DIRECTION_INDICATOR)) {
+			manager.requestValue(AutomotiveSignalId.FMS_DIRECTION_INDICATOR);
+		}
 		return movingDirection.getIntValue();
 	}
 
-	private void setMovingDirection(Uint8 movingDirection) {
-		this.movingDirection = movingDirection;
+	/** If the vehicle has an inserted driverscard.
+	 * @return true if a card is inserted. False otherwise
+	 */
+	public boolean hasDriverCard() {
+		if(!isRegistered(AutomotiveSignalId.FMS_DRIVER_1_CARD)) {
+			manager.requestValue(AutomotiveSignalId.FMS_DRIVER_1_CARD);
+		}
+		return (card.getIntValue() != 0);
 	}
 
-	private void setVehicleSpeed(SCSFloat vehicleSpeed) {
-		this.vehicleSpeed = vehicleSpeed;
+	/** Returns the state of the driver.
+	 * @return  An int with
+	            - 0: Rest
+				- 1: Driver available
+				- 2: Work
+				- 3: Drive
+				- 6: Error
+				- 7: Not available
+	 */
+	public int getWorkingState() {
+		if(!isRegistered(AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE)) {
+			manager.requestValue(AutomotiveSignalId.FMS_DRIVER_1_WORKING_STATE);
+		}
+		return (workingState.getIntValue());
 	}
 
-	private void setDistanceToService(SCSInteger distanceToService) {
-		this.distanceToService = distanceToService;
+	/** Indicates if the driver approaches or exceeds workings time limit.
+ 	 * @return An int with
+	            - 0: Normal
+				- 1: 15 min bef. 4 h 30 min
+				- 2: 4 h 30 min reached
+				- 3: 15 min bef. 9 h
+				- 4: 9 h reached
+				- 5: 15 min bef. 16 h
+				- 6: 16h reached
+				- 14: Error
+				- 15: Not available
+	 */
+	public int getTimeRelativeWorkingState() {
+		if(!isRegistered(AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES)) {
+			manager.requestValue(AutomotiveSignalId.FMS_DRIVER_1_TIME_REL_STATES);
+		}
+		return (timeRelativeWorkingState.getIntValue());
 	}
 
-	private void setInstantFuelEconomy(SCSFloat instantFuelEconomy) {
-		this.instantFuelEconomy = instantFuelEconomy;
+	// TODO Make method
+	/** Estimates how long until a refuel is recommended.
+	 * @return how many km until a stop for fueling is recommended. +- 10km
+	 */
+	public double getEstimatedKilometersUntilRefuel() {
+		if(!isMoving()) {
+
+		} else {
+
+		}
+		return 0.0;
 	}
 
-	private void setInstantaneousFuelConsumption(SCSFloat instantFuelConsumption) {
-		this.instantFuelConsumption = instantFuelConsumption;
+	// TODO Make method
+	/** Estimates how long until a timestop is recommended.
+	 * @return how many km until a stop for time is recommended. +- 10km
+	 */
+	public double getEstimatedKilometersUntilTimeStop() {
+		if(!isMoving()) {
+
+		} else {
+
+		}
+		return 0.0;
 	}
 
-	private void setMoving(Uint8 moving) {
-		this.isMoving = moving;
+	// Method that sets and determines start, stop and downTime.
+	// TODO how to use this method?
+	private void setTime() {
+		if(this.isMoving()) {
+			long tempStartTime = System.nanoTime();
+			if(driveStopTime != -1) {
+				downTime = tempStartTime - driveStopTime;
+				if(downTime >= THRESHOLD) {
+					driveStartTime = tempStartTime;
+				} else {
+					// Stop was too short. Do not change starttime.
+				}
+			} else {
+				driveStartTime = tempStartTime;
+			}
+
+		} else {
+			this.driveStopTime = System.nanoTime();
+		}
 	}
 
-	private void setFuel(SCSFloat fuelLevel) {
-		this.fuelLevel = fuelLevel;
+	// Method that determines if a signal is listened to.
+	private boolean isRegistered(int signalId) {
+		boolean isListenedAt = false;
+		Iterator<Integer> it = signalIDs.iterator();
+		while(it.hasNext()) {
+			if(it.next() == signalId) {
+				isListenedAt = true;
+				break;
+			}
+		}
+		return isListenedAt;
 	}
-
-
-
 }
