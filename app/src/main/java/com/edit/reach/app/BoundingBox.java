82c406542fcbe85a1d44825e493f11a1d1ef368a
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-10-06 17:09:13 +0200

    Removed RemoteTest

--- app/src/main/java/com/edit/reach/app/BoundingBox.java
+++ app/src/main/java/com/edit/reach/app/BoundingBox.java
@@ -1,8 +1,26 @@
 package com.edit.reach.app;
 
+import android.graphics.PointF;
+
+/**
+ * A square representing a geographical area.
+ */
 public class BoundingBox {
+    PointF bottomLeft;
+    PointF topRight;
 
-    public BoundingBox() {
+    public BoundingBox(PointF center, Float sideLength) {
+        float d = sideLength / 2;
 
+        bottomLeft = new PointF(center.x - d, center.y - d);
+        topRight = new PointF(center.x + d, center.x + d);
+    }
+
+    PointF getBottomLeft() {
+       return bottomLeft;
+    }
+
+    PointF getTopRight() {
+        return topRight;
     }
 }
