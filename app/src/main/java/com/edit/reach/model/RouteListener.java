871f6887aa3774fa560611d970e6b46124e678b8
Joakim Berntsson <jcberntsson@gmail.com>
2014-10-07 12:58:41 +0200

    Added a routelistener

---
+++ app/src/main/java/com/edit/reach/model/RouteListener.java
@@ -0,0 +1,12 @@
+package com.edit.reach.model;
+
+/**
+ * Created by Joakim on 2014-10-06.
+ */
+public interface RouteListener {
+
+    /**
+     * What the listener should do when the route has been initialized.
+     */
+    public void onInitialization();
+}
