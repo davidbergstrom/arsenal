bafa0f283848ba190f79889cf5909aef9fdc36f1
Joakim Berntsson <jcberntsson@gmail.com>
2014-10-07 12:57:46 +0200

    Reformatted Map and Route

--- app/src/main/java/com/edit/reach/model/Route.java
+++ app/src/main/java/com/edit/reach/model/Route.java
@@ -2,6 +2,9 @@ package com.edit.reach.model;
 
 import android.graphics.Color;
 import android.text.Html;
+import android.util.Log;
+import com.edit.reach.app.Remote;
+import com.edit.reach.app.ResponseHandler;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.*;
@@ -10,7 +13,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
-import java.lang.Integer;import java.lang.Math;import java.lang.String;import java.net.URLDecoder;
+import java.lang.Integer;import java.lang.Math;import java.lang.String;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -23,22 +29,98 @@ public class Route {
     private List<Leg> legs;
     private Circle endPointCircle;
     private Circle pointer; // Should this be an individual class (following a route)?
-
-    public Route(LatLng origin, LatLng destination){
-        String url = NavigationUtils.makeURL(origin, destination, null, true);
-        //new connectAsyncTask(url).execute();
-    }
-
-    public Route(JSONObject route) throws JSONException {
-        legs = new ArrayList<Leg>();
-        // Set route start and end locations as variables with getters
-        JSONArray arrayLegs = route.getJSONArray("legs");
-        for(int i = 0; i < arrayLegs.length(); i++) {
-            JSONObject legJSON = arrayLegs.getJSONObject(0);
-            legs.add(new Leg(legJSON));
+    private boolean initialized = false;
+    private List<RouteListener> listeners;
+    private List<IMilestone> milestones;
+    private ResponseHandler routeHandler = new ResponseHandler() {
+        @Override
+        public void onGetSuccess(JSONObject json) {
+            try {
+                JSONArray routeArray = json.getJSONArray("routes");
+                JSONObject route = routeArray.getJSONObject(0);
+                JSONArray arrayLegs = route.getJSONArray("legs");
+                for(int i = 0; i < arrayLegs.length(); i++) {
+                    JSONObject legJSON = arrayLegs.getJSONObject(0);
+                    legs.add(new Leg(legJSON));
+                }
+                alertListeners(); // Notify the observers that the route has been initialized
+                Log.d("Route", "Notified Observers");
+                initialized = true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
+
+        @Override
+        public void onGetFail() {
+
+        }
+    };
+
+    /**
+     * Create an empty route.
+     */
+    public Route(){
+        listeners = new ArrayList<RouteListener>();
+        legs = new ArrayList<Leg>();
+        milestones = new ArrayList<IMilestone>();
     }
 
+    /**
+     * Create a route with the provided origin and destination.
+     * @param origin, the start of the route
+     * @param destination, the end of the route
+     */
+    public Route(LatLng origin, LatLng destination){
+        this();
+        String stringUrl = NavigationUtils.makeURL(origin, destination, null, true);
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Remote.get(url, routeHandler);
+    }
+
+    /**
+     * Add a milestone to the route
+     * @param milestone, the milestone to add
+     */
+    public void addMilestone(IMilestone milestone){
+        milestones.add(milestone);
+        // Recalculate the route
+    }
+
+    /**
+     * Add all the milestones to the route
+     * @param milestones, the milestones to add
+     */
+    public void addMilestones(List<IMilestone> milestones){
+        this.milestones.addAll(milestones);
+        // Recalculate the route
+    }
+
+    /**
+     * Remove the milestone from the route
+     * @param milestone, the milestone to remove
+     */
+    public void removeMilestone(IMilestone milestone){
+        milestones.remove(milestone);
+        // Recalculate the route
+    }
+
+    /**
+     * Remove all of the routes milestones
+     */
+    public void removeAllMilestones(){
+        milestones.clear();
+    }
+
+    /**
+     * Draw this route on the map provided
+     * @param map, the map to draw the route on
+     */
     public void draw(GoogleMap map){
         for(Leg leg : legs){
             leg.draw(map);
@@ -51,36 +133,30 @@ public class Route {
                 .fillColor(Color.BLUE));
     }
 
+    /**
+     * Remove this route from all of its maps
+     */
     public void remove(){
         for(Leg leg : legs){
             leg.erase();
         }
+        this.removeListeners();
         this.endPointCircle.remove();
         this.pointer.remove();
     }
 
-    private LatLng evaluateNewLocation(LatLng rawLocation){
-        Leg currentLeg = legs.get(0);
-        Step nearestStep = currentLeg.steps.get(0);
-        for(Step step : currentLeg.steps){
-            LatLng location = step.startLocation;
-            LatLng nearestLocation = nearestStep.startLocation;
-            if(location.latitude - rawLocation.latitude <= nearestLocation.latitude - rawLocation.latitude &&
-                    location.longitude - rawLocation.longitude <= nearestLocation.longitude - rawLocation.longitude){
-                nearestStep = step;
-            }else{
-                break;
-            }
-        }
-
-        return nearestStep.startLocation;
-    }
-
+    /**
+     * Go to the provided location on the route.
+     * @param map, the map to use
+     * @param location, the location to move to
+     * @return the estimated location on the route
+     */
     public LatLng goTo(GoogleMap map, LatLng location){
         LatLng nearestLocation = legs.get(0).steps.get(0).subSteps.get(0);
         Step stepToRedraw = legs.get(0).steps.get(0);
 
         // TODO: Remake recursive!
+        // TODO: This only works if the substeps are in a relatively straight line
         outerLoop:
         for(Iterator<Leg> iteratorLeg = legs.iterator(); iteratorLeg.hasNext(); ){
             Leg leg = iteratorLeg.next();
@@ -90,17 +166,8 @@ public class Route {
                     LatLng subStep = step.subSteps.get(i);
                     LatLng subStepTwo = step.subSteps.get(i + 1);
 
-                    double a = Math.pow(Math.sin(Math.toRadians(location.latitude-subStep.latitude)/2), 2) +
-                            Math.cos(location.latitude) * Math.cos(subStep.latitude) *
-                                    Math.pow(Math.sin(Math.toRadians(location.longitude-subStep.longitude)/2), 2);
-                    double distance1 = getDistance(subStep, location); //R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-                    a = Math.pow(Math.sin(Math.toRadians(subStepTwo.latitude-location.latitude)/2), 2) +
-                            Math.cos(subStepTwo.latitude) * Math.cos(location.latitude) *
-                                    Math.pow(Math.sin(Math.toRadians(subStepTwo.longitude-location.longitude)/2), 2);
-                    double distance2 = getDistance(location, subStepTwo); //R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                    /*if(Math.abs(subStep.latitude - location.latitude) >= Math.abs(subStepTwo.latitude - location.latitude) &&
-                            Math.abs(subStep.longitude - location.longitude) >= Math.abs(subStepTwo.longitude - location.longitude)){*/
+                    double distance1 = getDistance(subStep, location);
+                    double distance2 = getDistance(location, subStepTwo);
 
                     if(distance2 <= distance1){
                         nearestLocation = subStepTwo;
@@ -112,17 +179,15 @@ public class Route {
                 }
                 step.erase();
                 iteratorStep.remove();
-                //leg.steps.remove(step);
             }
             leg.erase();
             iteratorLeg.remove();
-            //legs.remove(leg);
         }
 
         if(legs.size() == 0){
             // Route finished!
-
             endPointCircle.remove();
+
         }else{
             if(stepToRedraw != null){
                 stepToRedraw.redraw(map);
@@ -147,29 +212,55 @@ public class Route {
         return nearestLocation;
     }
 
+    /**
+     * Determines if the route has been initialized.
+     * @return true if the route has been initialized, false otherwise.
+     */
+    public boolean isInitialized(){
+        return initialized;
+    }
+
+    /**
+     * Add the listener to the route
+     * @param listener, the listener to add
+     */
+    public void addListener(RouteListener listener){
+        listeners.add(listener);
+    }
+
+    /**
+     * Remove all listeners from this route
+     */
+    public void removeListeners(){
+        listeners.clear();
+    }
+
+    private void alertListeners(){
+        for(RouteListener listener : listeners){
+            listener.onInitialization();
+        }
+    }
+
     private double getDistance(LatLng firstPosition, LatLng secondPosition){
         int R = 6371; // Earths radius in km
         double a = Math.pow(Math.sin(Math.toRadians(secondPosition.latitude-firstPosition.latitude)/2), 2) +
                 Math.cos(secondPosition.latitude) * Math.cos(firstPosition.latitude) *
                         Math.pow(Math.sin(Math.toRadians(secondPosition.longitude-firstPosition.longitude)/2), 2);
-        double distance1 = R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        return distance1;
+        return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     }
 
     private static double finalBearing(double lat1, double long1, double lat2, double long2){
-        return (_bearing(lat2, long2, lat1, long1) + 180.0) % 360;
-    }
-
-    private static double _bearing(double lat1, double long1, double lat2, double long2){
         double degToRad = Math.PI / 180.0;
         double phi1 = lat1 * degToRad;
         double phi2 = lat2 * degToRad;
         double lam1 = long1 * degToRad;
         double lam2 = long2 * degToRad;
 
-        return Math.atan2(Math.sin(lam2-lam1)*Math.cos(phi2),
+        double bearing = Math.atan2(Math.sin(lam2-lam1)*Math.cos(phi2),
                 Math.cos(phi1)*Math.sin(phi2) - Math.sin(phi1)*Math.cos(phi2)*Math.cos(lam2-lam1)) * 180/Math.PI;
+
+        return (bearing + 180.0) % 360;
     }
 
     /**
@@ -204,12 +295,19 @@ public class Route {
             }
         }
 
+        /**
+         * Draw this leg on the provided map
+         * @param map, the map to draw on
+         */
         public void draw(GoogleMap map){
             for(Step step : steps){
                 step.draw(map);
             }
         }
 
+        /**
+         * Erase the leg from all of its maps
+         */
         public void erase(){
             for(Step step : steps){
                 step.erase();
@@ -253,16 +351,27 @@ public class Route {
             }
         }
 
+        /**
+         * Draw the step on the provided map
+         * @param map, the map to draw on
+         */
         public void draw(GoogleMap map){
             //map.addMarker(new MarkerOptions().position(startLocation).title(distance+"m").snippet(instructions));
             polyline = map.addPolyline(new PolylineOptions().addAll(subSteps).width(12).color(Color.parseColor("#4411EE")));
         }
 
+        /**
+         * Redraw the step on the provided map
+         * @param map
+         */
         public void redraw(GoogleMap map){
             this.erase();
             polyline = map.addPolyline(new PolylineOptions().addAll(subSteps).width(12).color(Color.parseColor("#4411EE")));
         }
 
+        /**
+         * Erase the step from all of its maps
+         */
         public void erase(){
             polyline.remove();
         }
