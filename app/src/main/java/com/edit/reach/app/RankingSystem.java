3c8631296f65a735d09500dbc4a5a1f95c5db5f9
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-10-09 16:50:25 +0200

    Updated RankingSystem to communicate with MilestonesReceiver

--- app/src/main/java/com/edit/reach/app/RankingSystem.java
+++ app/src/main/java/com/edit/reach/app/RankingSystem.java
@@ -1,6 +1,7 @@
 package com.edit.reach.app;
 
 import com.edit.reach.model.Milestone;
+import com.edit.reach.model.MilestonesReceiver;
 import com.google.android.gms.maps.model.LatLng;
 import org.json.JSONObject;
 
@@ -10,31 +11,28 @@ import java.util.ArrayList;
 
 public class RankingSystem implements ResponseHandler {
 
-    private RankingSystem() {
+    MilestonesReceiver milestonesReceiver;
 
+    private RankingSystem(MilestonesReceiver milestonesReceiver) {
+        this.milestonesReceiver = milestonesReceiver;
     }
 
-    public ArrayList<Milestone> getMilestones(LatLng centralPoint, double sideLength) {
-        ArrayList<Milestone> milestones = new ArrayList<Milestone>();
+    public void getMilestones(LatLng centralPoint, double sideLength) {
 
         BoundingBox bbox = new BoundingBox(centralPoint, sideLength);
 
         try {
             URL url = WorldTruckerEndpoints.getMilestonesURL(bbox);
+            Remote.get(url, this);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
-        // https://api.worldtrucker.com/v1/
-        // tip?bbox=26.23952479477298%2C-94.9609375%2C57.77762234415841%2C12.96875
-        // &categories=
-
-        return milestones;
     }
 
     @Override
     public void onGetSuccess(JSONObject json) {
-
+        ArrayList<Milestone> milestones = new ArrayList<Milestone>();
+        milestonesReceiver.onMilestonesRecieved(milestones);
     }
 
     @Override
