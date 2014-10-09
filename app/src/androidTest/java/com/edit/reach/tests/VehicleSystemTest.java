bd64ed51296b993b429c130b46d0a0f968fb8f28
Tim Kerschbaumer <tim.kersch@gmail.com>
2014-10-09 13:57:43 +0200

    Added test files.

---
+++ app/src/androidTest/java/com/edit/reach/tests/VehicleSystemTest.java
@@ -0,0 +1,26 @@
+package com.edit.reach.tests;
+
+import junit.framework.TestCase;
+
+public class VehicleSystemTest extends TestCase {
+
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testGetKilometersUntilRefuel() throws Exception {
+		
+	}
+
+	public void testGetTimeUntilForcedRest() throws Exception {
+
+	}
+
+	public void testGetKilometersUntilService() throws Exception {
+
+	}
+
+	public void testGetVehicleState() throws Exception {
+
+	}
+}
--- app/src/main/java/com/edit/reach/app/MainActivity.java
+++ app/src/main/java/com/edit/reach/app/MainActivity.java
@@ -1,36 +1,72 @@
 package com.edit.reach.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.swedspot.automotiveapi.AutomotiveSignal;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.swedspot.automotiveapi.AutomotiveListener;
+import android.widget.TextView;
+import com.edit.reach.model.NavigationModel;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.swedspot.automotiveapi.*;
+import android.swedspot.automotiveapi.unit.*;
+import android.swedspot.automotiveapi.*;
+import android.swedspot.scs.data.*;
+import android.widget.TextView;
+
+
+import com.swedspot.vil.policy.*;
+import com.swedspot.vil.distraction.*;
 
 
 public class MainActivity extends Activity {
 
-    private AutomotiveListener hej = new AutomotiveListener() {
-        @Override
-        public void receive(AutomotiveSignal automotiveSignal) {
-
-        }
-
-        @Override
-        public void timeout(int i) {
-
-        }
-
-        @Override
-        public void notAllowed(int i) {
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	    super.onCreate(savedInstanceState);
+	    Log.d("ONCREATE", "ONCREATE");
+	    setContentView(R.layout.activity_main);
+	    NavigationModel m = new NavigationModel();
+
+	    // MORGAN EXAMPLE
+/*	    // Fire off an async task. Networking and similar should not (cannot) happen on the UI thread
+	    new AsyncTask() {
+		    @Override
+		    protected Object doInBackground(Object... objects) {
+			    // Access to Automotive API
+			    AutomotiveFactory.createAutomotiveManagerInstance(
+					    new AutomotiveCertificate(new byte[0]),
+					    new AutomotiveListener() { // Listener that observes the Signals
+						    @Override
+						    public void receive(final AutomotiveSignal automotiveSignal) {
+							    SCSFloat f = (SCSFloat)automotiveSignal.getData();
+							    Log.d("FUEL", f.getFloatValue() + "");
+						    }
+
+						    @Override
+						    public void timeout(int i) {}
+
+						    @Override
+						    public void notAllowed(int i) {}
+					    },
+					    new DriverDistractionListener() {       // Observe driver distraction level
+						    @Override
+						    public void levelChanged(final DriverDistractionLevel driverDistractionLevel) {
+						    }
+					    }
+			    ).register(AutomotiveSignalId.FMS_FUEL_LEVEL_1); // Register for the speed signal
+			    return null;
+		    }
+	    }.execute(); // And go!
+
+    }*/
+
     }
 
 
