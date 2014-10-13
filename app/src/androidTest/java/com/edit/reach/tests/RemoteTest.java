66731de732dc05a00b5086b31a3818186eba6881
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-10-13 20:24:20 +0200

    Set up some tests for RankingSystem

--- app/src/androidTest/java/com/edit/reach/tests/RemoteTest.java
+++ app/src/androidTest/java/com/edit/reach/tests/RemoteTest.java
@@ -1,10 +1,17 @@
 package com.edit.reach.tests;
 
+import android.util.Log;
 import junit.framework.TestCase;
 
 public class RemoteTest extends TestCase {
 
+    public void setUp() throws Exception {
+        super.setUp();
+        Log.d("RemoteTest", "SetUp");
+    }
+
     public void testGet() throws Exception {
+        Log.d("RemoteTest", "testGet");
 
     }
 }
