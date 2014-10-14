682938dd9c0d890ecdd56de8a3e93e0e57dab6de
Tobias Andersen <j.tobiasandersen@gmail.com>
2014-10-14 22:52:48 +0200

    Better setup method in RankingTest

--- app/src/androidTest/java/com/edit/reach/tests/RankingTest.java
+++ app/src/androidTest/java/com/edit/reach/tests/RankingTest.java
@@ -1,5 +1,6 @@
 package com.edit.reach.tests;
 
+import android.util.Log;
 import com.edit.reach.constants.GetStatus;
 import com.edit.reach.model.Milestone;
 import com.edit.reach.model.Ranking;
@@ -18,10 +19,11 @@ public class RankingTest extends TestCase {
 
     public void setUp() throws Exception {
         super.setUp();
+        Ranking.reset();
         failed = false;
     }
 
-    public void testGetMilestones() {
+    public void testLargeRadius() {
 
         Ranking.getMilestones(new MilestonesReceiver() {
             @Override
@@ -34,11 +36,12 @@ public class RankingTest extends TestCase {
             public void onMilestonesGetFailed() {
                 failed = true;
             }
-        }, new LatLng(62, 55), 60);
+        }, new LatLng(62, 55), 100);
 
-        while(Ranking.getStatus() != GetStatus.FINISHED) {
+        while(Ranking.getStatus() == GetStatus.RUNNING) {
             // Waiting for Remote to finsih
         }
+        Log.d("RankingTest", "testLargeRadius status is FINISHED");
 
         if (Ranking.getStatus() == GetStatus.SUCCEEDED) {
             assertNotNull(milestonesLists);
@@ -48,6 +51,48 @@ public class RankingTest extends TestCase {
             }
         } else if (Ranking.getStatus() == GetStatus.FAILED) {
            assertTrue(failed);
+        }
+
+        Log.d("RankingTest", "testLargeRadius is done");
+    }
+
+    public void testZeroRadius() {
+        Log.d("RankingTest", "starting testZeroRadius");
+        Ranking.getMilestones(new MilestonesReceiver() {
+            @Override
+            public void onMilestonesRecieved(ArrayList<IMilestone> ms) {
+                milestonesLists = new HashMap<Integer, ArrayList<IMilestone>>();
+                milestonesLists.put(2, ms);
+            }
+
+            @Override
+            public void onMilestonesGetFailed() {
+                failed = true;
+            }
+        }, new LatLng(62, 55), 0);
+
+        while(Ranking.getStatus() == GetStatus.RUNNING) {
+            // Waiting for Remote to finsih
+        }
+        Log.d("RankingTest", "testZeroRadius status is FINISHED");
+
+        if (Ranking.getStatus() == GetStatus.SUCCEEDED) {
+            Log.d("RankingTest", "SUCCEEDED");
+            assertNotNull(milestonesLists);
+
+            int results = milestonesLists.get(2).size();
+
+            assertEquals(0, results);
+            if (results > 0) {
+                assertSame(Milestone.class, milestonesLists.get(1).get(0).getClass());
+            } else {
+                Log.d("RankingTest", "0 results back");
+            }
+        } else if (Ranking.getStatus() == GetStatus.FAILED) {
+            Log.d("RankingTest", "FAILED");
+            assertTrue(failed);
+        } else {
+            Log.d("RankingTest", "" + Ranking.getStatus());
         }
     }
 }
