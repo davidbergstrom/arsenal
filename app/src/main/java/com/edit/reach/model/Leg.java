28b2b8df6286b2682debde8b896bcfe619633e33
Joakim Berntsson <jcberntsson@gmail.com>
2014-10-17 16:45:32 +0200

    Added new methods to Route, Leg, Step, Map

--- app/src/main/java/com/edit/reach/model/Leg.java
+++ app/src/main/java/com/edit/reach/model/Leg.java
@@ -1,6 +1,7 @@
 package com.edit.reach.model;
 
 import android.util.Log;
+import com.edit.reach.model.interfaces.IMilestone;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import org.json.JSONArray;
@@ -16,21 +17,22 @@ import java.util.List;
  */
 public class Leg {
     private List<Step> steps;
-    private long duration;	// Seconds
-    private long distance;	// Metres
-    private LatLng startLocation, endLocation;
+    //private long duration;	// Seconds
+    //private long distance;	// Metres
+    //private LatLng startLocation, endLocation;
+    private IMilestone endMilestone; // Milestones at the end of this leg
     private String DEBUG_TAG = "Leg";
 
     public Leg(JSONObject legJSON){
         Log.d(DEBUG_TAG, "Creating leg.");
         steps = new ArrayList<Step>();
         try {
-            distance = legJSON.getJSONObject("distance").getLong("value");
-            duration = legJSON.getJSONObject("duration").getLong("value");
-            JSONObject startPosition = legJSON.getJSONObject("start_location");
-            this.startLocation = new LatLng(startPosition.getDouble("lat"), startPosition.getDouble("lng"));
-            JSONObject endPosition = legJSON.getJSONObject("end_location");
-            this.endLocation = new LatLng(endPosition.getDouble("lat"), endPosition.getDouble("lng"));
+            //distance = legJSON.getJSONObject("distance").getLong("value");
+            long duration = legJSON.getJSONObject("duration").getLong("value");
+            //JSONObject startPosition = legJSON.getJSONObject("start_location");
+            //this.startLocation = new LatLng(startPosition.getDouble("lat"), startPosition.getDouble("lng"));
+            //JSONObject endPosition = legJSON.getJSONObject("end_location");
+            //this.endLocation = new LatLng(endPosition.getDouble("lat"), endPosition.getDouble("lng"));
 
             JSONArray stepsArray = legJSON.getJSONArray("steps");
             for(int i = 0; i < stepsArray.length(); i++){
@@ -41,6 +43,14 @@ public class Leg {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Set a milestone at the end of this leg
+     * @param milestone, the milestone this leg is heading to
+     */
+    void setMilestone(IMilestone milestone){
+        this.endMilestone = milestone;
     }
 
     /**
@@ -62,7 +72,12 @@ public class Leg {
         }
     }
 
-    public long getDuration() {
+    public float getDuration() {
+        float duration = 0;
+        for(Step step : steps){
+            duration += step.getDuration();
+            Log.d(DEBUG_TAG, "Step duration: " + step.getDuration());
+        }
         return duration;
     }
 
@@ -70,18 +85,20 @@ public class Leg {
         return steps;
     }
 
-    public long getDistance() {
+    public float getDistance() {
+        float distance = 0;
+        for(Step step : steps){
+            distance += step.getDistance();
+        }
         return distance;
     }
 
     public LatLng getStartLocation() {
-        return startLocation;
+        return steps.get(0).getStartLocation();
     }
 
     public LatLng getEndLocation() {
-        return endLocation;
+        return steps.get(steps.size()-1).getEndLocation();
     }
-
-
 
 }
