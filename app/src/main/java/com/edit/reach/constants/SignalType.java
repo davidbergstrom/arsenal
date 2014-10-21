33a08505e715b56eda8f88e47238a5e87a6e427c
Tim Kerschbaumer <tim.kersch@gmail.com>
2014-10-21 16:36:11 +0200

    Work on AISA

--- app/src/main/java/com/edit/reach/constants/SignalType.java
+++ app/src/main/java/com/edit/reach/constants/SignalType.java
@@ -20,5 +20,6 @@ public class SignalType {
 	public static final int TANK_SIZE_CALCULATED = 211;
 	public static final int LEG_UPDATE = 212;
     public static final int LEG_FINISHED = 213;
+	public static final int MILESTONE = 214;
 
 }
